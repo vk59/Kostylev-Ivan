27f7396949b418feb268ed70aeffa63f14b69fb4
vk59 <jkostylev592@gmail.com>
2021-06-04 19:39:44 +0300

    Initial commit.

---
+++ app/src/main/java/com/vk59/kostylev/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.vk59.kostylev
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.vk59.kostylev.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
